54a15abf1b09183832115beedd5395922e54587e
Charbel Chayeb <charbel.chayeb03@gmail.com>
2024-03-22 10:49:37 +0100

    beställningsform funcionalitet

--- startbootstrap-shop-homepage-gh-pages/js/scripts.js
+++ startbootstrap-shop-homepage-gh-pages/js/scripts.js
@@ -5,3 +5,117 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+
+/**
+ * a. Namnet är minst 2 tecken och max 50 tecken
+b. E-postadressen måste innehålla @ och max 50 tecken
+c. Telefonnummer får innehålla siffror, bindestreck och parenteser. Max 50 tecken.
+d. Leveransadress enligt svensk standard:
+i. Gatuadress: Min 2 tecken och Max 50 tecken
+ii. Postnummer: Exakt 5 siffror
+iii. Ort: Min 2 tecken och Max 50 tecken
+ */
+/**
+ * chackName().checkNr().checkEmail().checkAddress().checkZip().checkOrt()
+ */
+function checkFunc() {
+
+    /**
+     * 
+     * Checking Name here
+     *  Namnet är minst 2 tecken och max 50 tecken
+     */
+        const nameInput = document.getElementById('name');
+        const name = nameInput.value; // Retrieve the value of the input field
+        var accepted = false;
+    
+        if (name.length >= 2 && name.length <= 50) {
+            document.getElementById('nameError').innerHTML = ''; // Clear the error message
+            console.log(name);
+        } else {
+            document.getElementById('nameError').innerHTML = `
+            <div class="alert alert-danger my-2">Du måste ange mellan 2-50 bokstäver</div>`;
+        }
+    /**
+     * 
+     * Checking Number here
+     * Telefonnummer får innehålla siffror, bindestreck och parenteser. Max 50 tecken.
+     * 0731455803
+     */
+        const numberInput = document.getElementById('number').value;
+    
+        if (numberInput.length >=10 && numberInput.length <= 50) {
+            document.getElementById('numberError').innerHTML = ''; // Clear the error message
+            console.log(numberInput);
+        } else {
+            document.getElementById('numberError').innerHTML = `
+            <div class="alert alert-danger my-2">Du måste ange mellan 10-50 tecken</div>`;
+        }
+    
+     /**
+     * 
+     * Checking E-postadress
+     * E-postadressen måste innehålla @ och max 50 tecken
+     * 
+     */
+        const emailInput = document.getElementById('email').value;
+    
+        if (emailInput.length >=10 && emailInput.length <= 50 && emailInput.includes('@')) {
+            document.getElementById('emailError').innerHTML = ''; // Clear the error message
+            console.log(emailInput);
+        } else {
+            document.getElementById('emailError').innerHTML = `
+            <div class="alert alert-danger my-2">E-postadressen måste innehålla @ och max 50 tecken</div>`;
+        }
+    
+    /**
+     * 
+     * Checking address
+     * Gatuadress: Min 2 tecken och Max 50 tecken
+     * 
+     */
+        const addressInput = document.getElementById('address').value;
+    
+        if (addressInput.length >=2 && addressInput.length <= 50) {
+            document.getElementById('addressError').innerHTML = ''; // Clear the error message
+            console.log(addressInput);
+        } else {
+            document.getElementById('addressError').innerHTML = `
+            <div class="alert alert-danger my-2">Addressen måste innehålla mellan 2-50 tecken</div>`;
+        }
+    
+    /**
+     * 
+     * Checking Zip
+     * Postnummer: Exakt 5 siffror
+     * 
+     */
+    const zipInput = document.getElementById('zip').value;
+    
+    if (zipInput.length == 5) {
+        document.getElementById('zipError').innerHTML = ''; // Clear the error message
+        console.log(zipInput);
+    } else {
+        document.getElementById('zipError').innerHTML = `
+        <div class="alert alert-danger my-2">Postnummer måste innehåla exakt 5 siffror</div>`;
+    }
+    
+    /**
+     * 
+     * Checking locality
+     * Ort: Min 2 tecken och Max 50 tecken
+     * 
+     */
+    const localityInput = document.getElementById('locality').value;
+    
+    if (localityInput.length >= 2 && localityInput.length <= 50) {
+        document.getElementById('localityError').innerHTML = ''; // Clear the error message
+        console.log(localityInput);
+    } else {
+        document.getElementById('localityError').innerHTML = `
+        <div class="alert alert-danger my-2">Du måste ange mellan 2-50 tecken</div>`;
+    }
+    
+    }
+    
